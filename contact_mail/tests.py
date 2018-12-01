1b4e4945ca6d7c78a1c409f38eb610bcbfe84a5b
miesorozec <konrad.szpyrek@gmail.com>
2018-12-01 17:39:42 +0100

    add travis base config

--- contact_mail/tests.py
+++ contact_mail/tests.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 from random import randint
+
+
 # Create your tests here.
 
 class CiTest(TestCase):
     def test_test(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 
     def test_test2(self):
-        self.assertEqual(1,2)
+        self.assertEqual(1, 2)
