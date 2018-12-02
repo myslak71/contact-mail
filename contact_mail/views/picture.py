28df6f2589d92393238fbaf230b75f312868b7cc
Kornel Szurek <konrad.szpyrek@gmail.com>
2018-12-02 17:21:43 +0100

    Frontend4

--- contact_mail/views/picture.py
+++ contact_mail/views/picture.py
@@ -8,9 +8,12 @@ from contact_mail.views.base import BaseView
 
 class AddPictureView(View):
     def post(self, request, id):
-        if request.FILES['picture_file']:
-            print(request.FILES['picture_file'])
-            contact = Person.objects.get(pk=id)
-            contact.image = request.FILES['picture_file']
-            contact.save()
+        try:
+            if request.FILES['picture_file']:
+                print(request.FILES['picture_file'])
+                contact = Person.objects.get(pk=id)
+                contact.image = request.FILES['picture_file']
+                contact.save()
+        except:
+            messages.add_message(request, messages.ERROR, 'You need to specify file path.')
         return redirect('/modify/{}'.format(id))
