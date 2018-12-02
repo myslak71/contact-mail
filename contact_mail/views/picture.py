33accdc3f53c0316e509055140c5fc43046ed7ed
Kornel Szurek <konrad.szpyrek@gmail.com>
2018-12-02 20:09:50 +0100

    Frontend4

--- contact_mail/views/picture.py
+++ contact_mail/views/picture.py
@@ -1,9 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views import View
-from django.core.files.storage import FileSystemStorage
 from contact_mail.models import Group, Person
-from contact_mail.views.base import BaseView
 
 
 class AddPictureView(View):
