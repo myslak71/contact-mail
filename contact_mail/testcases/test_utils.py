e0def042db0968f061998ffbbcbb2db9b8e6001b
miesorozec <konrad.szpyrek@gmail.com>
2018-12-02 09:03:55 +0100

    more tests

--- contact_mail/testcases/test_utils.py
+++ contact_mail/testcases/test_utils.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+
 from contact_mail.utils import *
 
 
@@ -10,7 +11,18 @@ class UtilsTest(TestCase):
         self.assertFalse(check_email('123com.pl'))
 
     def test_valid_number(self):
-        self.assertEqual(check_if_unsigned_int(2),2)
+        self.assertEqual(check_if_unsigned_int(2), 2)
 
     def test_invalid_number(self):
         self.assertEqual(check_if_unsigned_int(-2), None)
+
+    def test_contact_exist(self):
+        Person.objects.create(name='Name', surname='Surname', description='Description').save()
+        c = contact_exist_get(1)
+        self.assertEqual(c.name, 'Name')
+        self.assertEqual(c.surname, 'Surname')
+        self.assertEqual(c.description, 'Description')
+
+    def test_contact_not_exist(self):
+        with self.assertRaises(Http404):
+            contact_exist_get(1)
