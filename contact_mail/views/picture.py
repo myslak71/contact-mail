1bcfc3fd16569f9634b693ff461817f991df39f4
Kornel Szurek <konrad.szpyrek@gmail.com>
2018-12-03 00:06:25 +0100

    Frontend4

--- contact_mail/views/picture.py
+++ contact_mail/views/picture.py
@@ -1,16 +1,22 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.views import View
 from contact_mail.models import Group, Person
+from contact_mail.views.base import BaseView
 
 
-class AddPictureView(View):
+class AddPictureView(BaseView):
+    FORM_FIELDS = ('picture_file',)
     def post(self, request, id):
-        try:
-            if request.FILES['picture_file']:
-                contact = Person.objects.get(pk=id)
-                contact.image = request.FILES['picture_file']
-                contact.save()
-        except:
-            messages.add_message(request, messages.ERROR, 'You need to specify a file path.')
+        self._get_form_field_values(request)
+
+        if not self._validate_form(request, *self.FORM_FIELDS):
+            return redirect('/modify/{}'.format(id))
+
+        contact = Person.objects.filter(pk=id).first()
+
+        if not contact:
+            messages.add_message(request, messages.ERROR, 'Contact does not exist')
+            return redirect('/')
+        contact.image = self.picture_file
+        contact.save()
         return redirect('/modify/{}'.format(id))
